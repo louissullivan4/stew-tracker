7279253540e9f055d9bf5329585f1ef2aa7eb89d
Louis Sullivan <sullivanlouis0@gmail.com>
2023-05-22 19:09:14 +0100

    added update and create food

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
-
 const db = require('./dbquery')
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+app.get('/', (request, response) => {
+    response.send('Hello from steW food tracker!')
+});
 
 app.get('/food/:foodName', db.getFoodByName)
 
 app.get('/foodlike/:foodName', db.getFoodLikeName)
 
+app.post('/createfood/', db.createFood)
+
+app.put('/updatefoodname/:foodName', db.updateFoodByName)
 
 
 app.listen(port, () => {
--- dbquery.js
+++ dbquery.js
@@ -1,9 +1,7 @@
 const pool = require('./keys')
 
 const getFoodByName= (request, response) => {
-    //capitalize the first letter of every word
     const foodName = request.params.foodName.replace(/\b\w/g, l => l.toUpperCase());
-    // check for spaces
     if (foodName.includes('%20')) {
         foodName.replace('%20', ' ')
     }
@@ -22,16 +20,53 @@ const getFoodLikeName = (request, response) => {
         foodName.replace('%20', ' ')
     }
     const searchTerm = `%${foodName}%`;
-    // exclude duplicates
-    pool.query('SELECT DISTINCT * FROM stewfood WHERE name ILIKE $1', [searchTerm], (error, results) => {
+    pool.query('SELECT DISTINCT name, serving, calories FROM stewfood WHERE name ILIKE $1', [searchTerm], (error, results) => {
         if (error) {
           throw error
         }
+        console.log(results.rows)
         response.status(200).json(results.rows)
     })
 }
 
+const createFood = (request, response) => {
+    pool.query('SELECT fid FROM stewfood ORDER BY fid DESC LIMIT 1', (error, results) => {
+        if (error) {
+            throw error
+        }
+        incFid = results.rows[0].fid + 1
+        const { name, serving, calories } = request.body
+        pool.query('INSERT INTO stewfood (fid, name, serving, calories) VALUES ($1, $2, $3, $4) RETURNING *', [incFid, name, serving, calories], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Food added: ${results.rows[0].name}`)
+        })
+    })
+}
+
+const updateFoodByName = (request, response) => {
+    const foodName = request.params.foodName.replace(/\b\w/g, l => l.toUpperCase());
+    if (foodName.includes('%20')) {
+        foodName.replace('%20', ' ')
+    }
+    const { name, serving, calories } = request.body
+    pool.query(
+        'UPDATE stewfood SET name = $1, serving = $2, calories = $3 WHERE name = $4',
+        [name, serving, calories, foodName],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`Food modified: ${name}`)
+        }
+    )
+}
+
+ 
 module.exports = {
     getFoodByName,
-    getFoodLikeName
+    getFoodLikeName,
+    createFood,
+    updateFoodByName
 }
